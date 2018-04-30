6b350a142f743fa7a10e057a4ac9566fc95d4528
Alan Trope <atrope@users.noreply.github.com>
2018-04-30 12:33:59 +0300

    Add project files.

---
+++ Selectlist/Selectlist.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <Windows.h>
+using namespace std;
+struct item {
+	string name;
+	bool selected;
+};
+class Selectlist {
+private:
+	COORD position;
+	vector<item> list;
+	int arrow;
+	void updateLine(int line, int line2);
+public:
+	Selectlist(COORD pos, int opt = 3) : position(pos), arrow(0) {
+		for (int i = 0; i < opt; i++) this->list.push_back({ "Option #" + to_string(i) , false });
+		draw();
+	};
+	void draw();
+	void goUp();
+	void goDown();
+	void choose();
+	~Selectlist() {};
+};
